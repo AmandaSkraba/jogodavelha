ec7652a1af0a06e0cbf91e6577935cf585a85ea9
gonca <gonca@EDVI5>
2024-09-26 15:54:07 -0300

    Adicionar arquivos de projeto.

---
+++ jogodavelha/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace jogodavelha
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ jogodavelha/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace jogodavelha
+{
+    public partial class MainPage : ContentPage
+    {
+        string vez = "X";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if(vez == "X") {
+                btn.Text = "X";
+                vez = "O";
+
+            } else {
+                btn.Text = "O";
+                vez = "X";
+            }
+
+            /*Verificando se x ganhou na primeira linha*/
+
+            if (btn10.Text == "X" && btn11.Text == "X" && btn12.Text == "X") {
+                DisplayAlert("Parabéns!", "O X ganhou", "OK");
+            }
+        }
+    }
+
+}
